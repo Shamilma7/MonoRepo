eb1c7e6fbf0bdc632b803dc5851ae8783634db6d
Shamil Magamadov <shamil_magomadov@hotmail.com>
2023-03-08 19:01:09 +0100

    remove garbage

--- apps/russian-to-chechen/src/main/kotlin/com/russiantochechen/dictionary/erwin/ErwinTranslatorService.kt
+++ apps/russian-to-chechen/src/main/kotlin/com/russiantochechen/dictionary/erwin/ErwinTranslatorService.kt
@@ -7,67 +7,6 @@ import org.springframework.stereotype.Service
 class ErwinTranslatorService(
     private val dictionary: Dictionary = Dictionary()
 ) {
-    fun translate(text: String): String {
-        val sentences = text.split("(?<=[.!?])\\s+".toRegex())
-
-        val translatedSentences = mutableListOf<List<String>>()
-
-        for (sentence in sentences) {
-            val words = sentence.split("\\s+".toRegex())
-
-        }
-
-        return translatedSentences.joinToString(" ") { words ->
-            words.joinToString(" ")
-        }
-    }
-
-
-    fun translateClean(text: String): String {
-        val sentences = text.split("(?<=[.!?])\\s+".toRegex())
-
-
-        val translatedSentences = sentences.map { sentence ->
-            sentence.split("\\s+".toRegex()).mapIndexedNotNull { i, word ->
-                var translation: String? = null
-                var j = i + 1
-                while (j <= sentence.split("\\s+".toRegex()).size - 1 && translation == null) {
-                    val nextWords = (i..j).map { sentence.split("\\s+".toRegex())[it] }.joinToString(" ")
-                    translation = dictionary.toChechen(nextWords)
-                    j++
-                }
-                translation
-            }
-        }
-
-
-        return translatedSentences.joinToString(" ") { words ->
-            words.joinToString(" ")
-        }
-    }
-
-    fun translateEvenCleaner(text: String): String {
-        val sentences = text.split("(?<=[.!?])\\s+".toRegex())
-        val translatedSentences = sentences.map { sentence ->
-            val words = sentence.split("\\s+".toRegex())
-            words.flatMap { word ->
-                val cleanWord = getCleaned(word)
-                generateSequence(cleanWord) { nextWord ->
-                    if (nextWord == getCleaned(words.last())) null
-                    else "$nextWord ${getCleaned(words[words.indexOf(nextWord) + 1])}"
-                }.mapNotNull { phrase ->
-                    val punctuation = getPunctuation(word)
-                    dictionary.toChechen(phrase)?.let { "$it$punctuation" }
-                }.toList()
-            }
-        }
-
-
-        return translatedSentences.joinToString(" ") { words ->
-            words.joinToString(" ")
-        }
-    }
-
     fun translateFinal(text: String): String {
         val sentences = text.split("(?<=[.!?])\\s+".toRegex())
         val translatedSentences = sentences.map { sentence ->
@@ -111,30 +50,4 @@ class ErwinTranslatorService(
     fun getCleaned(word: String) = word.replace(Regex("[.,?]+"), "")
 
     fun getPunctuation(word: String) = word.replace(Regex("[\\w\\d]+"), "")
-
-    /*
-    * // If exact match not found, try to find longer phrases
-        val words = phrase.split(" ")
-        for (i in words.indices) {
-            val phrase = words.subList(i, words.size).joinToString(" ")
-            val match = findEntriesWith(definitionText = phrase).firstOrNull()?.lexicalUnit
-            if (match != null) {
-                return match
-            }
-        }*/
-
-    private fun clean(word: String) = word.removeSuffix(".")
-        .removeSuffix(",")
-
-
-    private fun formatChechenWord(russianWord: String, chechenWord: String): String {
-        var result = chechenWord
-        if (russianWord.contains(".")) {
-            result += ".\n"
-        }
-        if (russianWord.contains(",")) {
-            result += ","
-        }
-        return result
-    }
 }
